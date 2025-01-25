44c8fe2e62b0ca06750e6c53797dc00a25d4cdef
RoelofNeuman <roelofneuman@gmail.com>
2025-01-25 15:10:22 +0100

    double stream
    changed script to be dependant on the players distance. two stream now added

--- Bubble Sumo (GGJ225)/Assets/scripts Roel/BububleControllerScript.cs
+++ Bubble Sumo (GGJ225)/Assets/scripts Roel/BububleControllerScript.cs
@@ -3,23 +3,35 @@ using System.Collections;
 
 public class BububleControllerScript : MonoBehaviour
 {
-    public ParticleSystem bubbleParticles;
-    public float playerDistance;
+    public ParticleSystem bubbleParticlesOne;
+    public ParticleSystem bubbleParticlesTwo;
+    private GameObject playerOne;
+    private GameObject playerTwo;
+    private float playerDistance;
     private float bubbleDuration;
   
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
-        bubbleDuration = bubbleParticles.main.startLifetime.constant;
+        bubbleDuration = bubbleParticlesOne.main.startLifetime.constant;
+        playerOne = GameObject.FindGameObjectWithTag("Player1");
+        playerTwo = GameObject.FindGameObjectWithTag("Player2");
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Calculate Distance between players
+        playerDistance = Vector3.Distance(playerOne.transform.position, playerTwo.transform.position);
         float playerMiddleDistance = playerDistance / 2;
-        var main = bubbleParticles.main;
-        main.startSpeed = playerMiddleDistance / bubbleDuration;
-        
+
+
+        var mainOne = bubbleParticlesOne.main;
+        mainOne.startSpeed = playerMiddleDistance / bubbleDuration;
+        var mainTwo = bubbleParticlesTwo.main;
+        mainTwo.startSpeed = playerMiddleDistance / bubbleDuration;
+
+
     }
 }
