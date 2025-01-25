82b3b2b33ec0beb9432d61b50d14deace98fc75c
RoelofNeuman <roelofneuman@gmail.com>
2025-01-25 14:18:44 +0100

    bubble start + script folder

---
+++ Bubble Sumo (GGJ225)/Assets/scripts Roel/BububleControllerScript.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+
+public class BububleControllerScript : MonoBehaviour
+{
+    public ParticleSystem bubbleParticles;
+    public float playerDistance;
+    private float bubbleDuration;
+  
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        bubbleDuration = bubbleParticles.main.startLifetime.constant;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float playerMiddleDistance = playerDistance / 2;
+        var main = bubbleParticles.main;
+        main.startSpeed = playerMiddleDistance / bubbleDuration;
+        
+    }
+}
