91ee75fe541ab353dc107a8feef770a9b2ca7990
SarahHop <sarahandelisabeth@gmail.com>
2025-01-26 12:23:18 +0000

    scenes done ish

---
+++ Bubble Sumo (GGJ225)/Assets/Scenes/ScriptsSarah/MainMenu.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void mainMenu()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void SettingsGo()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+
+    }
+
+
+}
---
+++ Bubble Sumo (GGJ225)/Assets/Scenes/ScriptsSarah/SettingsMenu.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void setVolume(float volume)
+    {
+        audioMixer.SetFloat("volume",volume);
+    }
+
+}
