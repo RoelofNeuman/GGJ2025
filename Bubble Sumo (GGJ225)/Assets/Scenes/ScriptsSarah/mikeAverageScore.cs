5f8c0529fbf483c136c98facfbe3381b7eefe9d4
RoelofNeuman <roelofneuman@gmail.com>
2025-01-26 15:04:18 +0100

    average score in mikeinput

--- Bubble Sumo (GGJ225)/Assets/Scenes/ScriptsSarah/mikeAverageScore.cs
+++ Bubble Sumo (GGJ225)/Assets/Scenes/ScriptsSarah/mikeAverageScore.cs
@@ -1,8 +1,11 @@
 using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class mikeAverageScore : MonoBehaviour
 {
+    public List<string> mikeList = new List<string>(); //list from mic devices 
     public string micDevice1; // Microphone device name for Player 1
     public string micDevice2; // Microphone device name for Player 2
 
@@ -16,8 +19,9 @@ public class mikeAverageScore : MonoBehaviour
 
     private void Start()
     {
-        StartCoroutine(StartMicInput(micDevice1, 1));
-        StartCoroutine(StartMicInput(micDevice2, 2));
+        PopulateMic();
+        StartCoroutine(StartMicInput(micDevice1, 0));
+        StartCoroutine(StartMicInput(micDevice2, 1));
     }
 
     private void Update()
@@ -25,8 +29,8 @@ public class mikeAverageScore : MonoBehaviour
         if (mic1Started && mic2Started)
         {
             // Calculate microphone volumes
-            float micVolume1 = GetMicrophoneVolume(micInput1, micDevice1);
-            float micVolume2 = GetMicrophoneVolume(micInput2, micDevice2);
+            float micVolume1 = GetMicrophoneVolume(micInput1, micDevice1)*25f;
+            float micVolume2 = GetMicrophoneVolume(micInput2, micDevice2)*25f;
 
             // Determine the winner
             if (micVolume1 > micVolume2)
@@ -39,7 +43,8 @@ public class mikeAverageScore : MonoBehaviour
             }
             else
             {
-                Debug.Log("It's a tie!");
+                Debug.Log("1: " + micVolume1);
+                Debug.Log("2: " + micVolume2);
             }
         }
     }
@@ -57,12 +62,12 @@ public class mikeAverageScore : MonoBehaviour
             }
 
             // Assign the microphone input to the correct variable
-            if (playerIndex == 1)
+            if (playerIndex == 0)
             {
                 micInput1 = micInput;
                 mic1Started = true;
             }
-            else if (playerIndex == 2)
+            else if (playerIndex == 1)
             {
                 micInput2 = micInput;
                 mic2Started = true;
@@ -76,26 +81,46 @@ public class mikeAverageScore : MonoBehaviour
         }
     }
 
+    private void PopulateMic()
+    {
+        //clear existing list of device check new available ones 
+        mikeList.Clear();
+
+        foreach (var device in Microphone.devices)
+        {
+            mikeList.Add(device);
+        }
+        micDevice1 = mikeList[0];
+        micDevice2= mikeList[1];
+
+    }
+
     private float GetMicrophoneVolume(AudioClip micInput, string micDevice)
     {
+        //return 0 if no mic available 
         if (micInput == null)
-            return 0f;
-
-        float[] samples = new float[sampleWindow];
-        int micPosition = Microphone.GetPosition(micDevice) - sampleWindow + 1;
-
-        if (micPosition < 0)
-            return 0f;
-
-        micInput.GetData(samples, micPosition);
-
-        float sum = 0f;
-        for (int i = 0; i < sampleWindow; i++)
         {
-            sum += samples[i] * samples[i];
+            return 0f;
+            
+        }
+        float[] sample = new float[sampleWindow]; //buffer hold audio samples 
+        int micPosition = Microphone.GetPosition(micDevice) - sampleWindow + 1; //get mic pos
+
+        //if mic pos invalid, return 0 
+        if (micPosition < 0)
+        { return 0f;
         }
 
-        return Mathf.Sqrt(sum / sampleWindow); // RMS value
+        micInput.GetData(sample, micPosition); //fetch audio data into the buffer 
+
+        //calc avrg volume from sample 
+        float sum = 0f;
+
+        for (int i = 0; i < sampleWindow; i++)
+        {
+            sum += sample[i] * sample[i];
+        }
+        return Mathf.Sqrt(sum / sampleWindow); //return RMS value of the sample 
     }
 
     private void OnApplicationQuit()
